10a63512b90d97d828865c691aa1896b9704a565
Adaline Simonian <adalinesimonian@gmail.com>
2024-02-12 14:20:29 -0800

    fix: handle different envs when getting build head

--- copy-head.mjs
+++ copy-head.mjs
@@ -1,4 +1,25 @@
-import { copyFile, mkdir } from 'fs/promises';
+import { copyFile, mkdir, writeFile, readFile } from 'fs/promises';
 
 await mkdir('dist', { recursive: true });
-await copyFile('.git/ORIG_HEAD', 'dist/BUILD_HEAD');
+
+try {
+  await copyFile('.git/ORIG_HEAD', 'dist/BUILD_HEAD');
+
+  console.log(
+    `Build head ${await readFile('dist/BUILD_HEAD', 'utf8')} written to dist/BUILD_HEAD`,
+  );
+} catch (error) {
+  if (error.code === 'ENOENT') {
+    if (process.env.SOURCE_VERSION) {
+      await writeFile('dist/BUILD_HEAD', process.env.SOURCE_VERSION);
+
+      console.log(
+        `Build head ${process.env.SOURCE_VERSION} written to dist/BUILD_HEAD`,
+      );
+    } else {
+      console.warn('The file .git/ORIG_HEAD does not exist.');
+    }
+  } else {
+    console.error('An error occurred while copying the file:', error);
+  }
+}
