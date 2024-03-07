3530f41c8da0f33da7ef29de650293a4cd06c950
Adaline Simonian <adalinesimonian@gmail.com>
2024-03-07 12:15:47 -0800

    fix: no colour logs in prod

--- packages/uib-ord-sync/src/main.ts
+++ packages/uib-ord-sync/src/main.ts
@@ -3,8 +3,15 @@ import { AppModule } from './app.module';
 import { isProd } from 'ordbokapi-common';
 
 async function main() {
+  const prod = isProd();
+
+  // Disable colour logging in production
+  if (prod) {
+    process.env.NO_COLOR = 'true';
+  }
+
   await NestFactory.createApplicationContext(AppModule, {
-    logger: isProd()
+    logger: prod
       ? ['log', 'warn', 'error', 'fatal']
       : ['verbose', 'debug', 'log', 'warn', 'error', 'fatal'],
   });
